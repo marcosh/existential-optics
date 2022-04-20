5bac128d274f50c2302362ac99279a85b96b31a2
Marco Perone <marco.perone@tweag.io>
2022-04-20 10:44:10 +0200

    common prism operations

--- src/Prism.hs
+++ src/Prism.hs
@@ -28,3 +28,21 @@ _Right = morph _RightPrism
   where
     _RightPrism :: Prism (Either c a) (Either c b) a b
     _RightPrism = Optic id id
+
+-- COMMON OPERATIONS
+
+preview :: Morph f Either => Optic f s t a b -> s -> Maybe a
+preview = previewPrism . morph
+  where
+    previewPrism :: Prism s t a b -> s -> Maybe a
+    previewPrism (Optic f _) = rightToMaybe . f
+
+    rightToMaybe :: Either a b -> Maybe b
+    rightToMaybe (Left  _) = Nothing
+    rightToMaybe (Right b) = Just b
+
+review :: Morph f Either => Optic f s t a b -> b -> t
+review = reviewPrism . morph
+  where
+    reviewPrism :: Prism s t a b -> b -> t
+    reviewPrism (Optic _ g) = g . Right
