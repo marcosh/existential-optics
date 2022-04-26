e10a0d5e70fa2fefe4f620ad69004aeed17a1bc1
Marco Perone <marco.perone@tweag.io>
2022-04-26 10:22:30 +0200

    fix length of vector in traversal

--- src/PowerSeries.hs
+++ src/PowerSeries.hs
@@ -1,24 +1,10 @@
-{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 module PowerSeries where
 
-import Data.Bifunctor
+import TypeTuples
 
-data PowerSeries a x
-  = Const a
-  | Poly  (PowerSeries a x) x
+-- fixed-vector
+import Data.Vector.Fixed.Boxed
 
-instance Bifunctor PowerSeries where
-  first :: (a -> b) -> PowerSeries a x -> PowerSeries b x
-  first f (Const a)   = Const (f a)
-  first f (Poly ps x) = Poly (first f ps) x
-
-  second :: (x -> y) -> PowerSeries a x -> PowerSeries a y
-  second f (Const a)   = Const a
-  second f (Poly ps x) = Poly (second f ps) (f x)
-
-actionProduct :: Semigroup x => (a -> b -> c) -> PowerSeries a x -> PowerSeries b x -> PowerSeries c x
-actionProduct f (Const a)     (Const b)      = Const $ f a b
-actionProduct f (Const a)     (Poly psbx x)  = Poly (first (f a) psbx) x
-actionProduct f (Poly psax x) (Const b)      = Poly (first (flip f b) psax) x
-actionProduct f (Poly psax x) (Poly psbx x') = Poly (actionProduct f psax psbx) (x <> x')
+newtype PowerSeries a x = PowerSeries (Snd a, Vec (Fst a) x)
--- src/Traversal.hs
+++ src/Traversal.hs
@@ -6,14 +6,3 @@ import PowerSeries
 -- DATA TYPE
 
 type Traversal = Optic PowerSeries
-
--- instance ExistentiallyAssociative PowerSeries where
---   type E PowerSeries a b = PowerSeries a b
---   type C PowerSeries a b c = Semigroup c
-
---   existentialAssociateL :: Semigroup c => PowerSeries a (PowerSeries b c) -> PowerSeries (PowerSeries a b) c
---   existentialAssociateL (Const a)           = Const (Const a)
---   existentialAssociateL (Poly psapsbc psbc) = actionProduct (\ps b -> _wB) (existentialAssociateL psapsbc) psbc
-
---   existentialAssociateR :: PowerSeries (PowerSeries a b) c -> PowerSeries a (PowerSeries b c)
---   existentialAssociateR ps = _
